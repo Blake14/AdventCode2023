e1bb0e2fc770f533a30135e827286029aba4f4ec
Blake14 <blakemcpherson2@gmail.com>
2023-12-11 16:39:36 -0600

    style: advent code styling

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div
+      style={{
+        height: "100%",
+        width: "100%",
+        padding: 20,
+      }}
+    >
+      HomePage
+    </div>
+  );
+};
+
+export default HomePage;
